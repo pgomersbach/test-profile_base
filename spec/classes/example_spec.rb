8ba5c946a09ea6c4e939ede99374f7f5148052b1
Paul Gomersbach <p.gomersbach@rely.nl>
2015-06-16 18:20:11 +0200

    fix concat_basedir fact

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -5,16 +5,15 @@ describe 'profile_base' do
     on_supported_os.each do |os, facts|
       context "on #{os}" do
         let(:facts) do
-          facts
+          facts.merge({
+            :concat_basedir => "/foo"
+          })
         end
 
         context "profile_base class without any parameters" do
           let(:params) {{ }}
 
           it { is_expected.to compile.with_all_deps }
-
-  
-
         end
       end
     end
